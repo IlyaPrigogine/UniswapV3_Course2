325e7062eb0aa9a30076f15a03a412f235705fad
IlyaPrigogine <prigogine@protonmail.com>
2021-12-03 13:31:22 +0800

    added dai_func.ts

--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -1,4 +1,6 @@
-import {deployments, ethers, getNamedAccounts} from 'hardhat';
+import {deployments, ethers, getNamedAccounts,network} from 'hardhat';
+import {IERC20, MyDefiProject} from "../typechain";
+import {getContract, getContractAt} from "@nomiclabs/hardhat-ethers/dist/src/helpers";
 
 const {execute, read, get} = deployments;
 
@@ -6,10 +8,23 @@ async function main() {
 
     const {owner} = await getNamedAccounts();
 
-    const MyDefiProject = await get('MyDefiProject');
-    console.log(`MyDefiProject: ${MyDefiProject.address}`);
+    const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
+    console.log(`MyDefiProject: ${await MyDefiProject.address}`);
+
+    if (await network.name === 'ropsten') {
 
 
+
+
+        // const dai = await ethers.getContractAt<IERC20>('IERC20',dai_ropsten);
+        // await dai.approve(MyDefiProject.address,parseEther(initialApproveAmount));
+        //
+        // const weth = await ethers.getContractAt<IERC20>('IERC20',weth_ropsten);
+        // await weth.approve(MyDefiProject.address,parseEther(initialApproveAmount));
+
+        // await MyDefiProject.swapExactInputSingle(parseEther(daiAmountIn));
+        // await MyDefiProject.swapExactOutputSingle(parseEther(wethAmountOut),parseEther(daiAmountInMaximum));
+    }
 }
 
 
