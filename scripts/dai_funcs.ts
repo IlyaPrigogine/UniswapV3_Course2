83fc74a5ee0b30136030c419e62f86541dbaa6a5
IlyaPrigogine <prigogine@protonmail.com>
2021-12-03 13:57:54 +0800

    updated dai_funcs

--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -2,7 +2,7 @@ import {deployments, ethers, getNamedAccounts,network} from 'hardhat';
 import {IERC20, MyDefiProject} from "../typechain";
 import {getContract, getContractAt} from "@nomiclabs/hardhat-ethers/dist/src/helpers";
 import {parseEther} from "ethers/lib/utils";
-import {daiAmountIn} from "../Helpers/constants";
+import {daiAmountIn, daiAmountInMaximum, wethAmountOut} from "../Helpers/constants";
 
 const {execute, read, get} = deployments;
 
@@ -15,6 +15,12 @@ async function main() {
 
     if (await network.name === 'ropsten') {
         await MyDefiProject.swapExactInputSingle(parseEther(daiAmountIn));
+        await MyDefiProject.swapExactOutputSingle(parseEther(wethAmountOut),parseEther(daiAmountInMaximum));
+
+        await MyDefiProject.swapExactInputMultihop(parseEther(daiAmountIn));
+        await MyDefiProject.swapExactInputMultihopV2(parseEther(daiAmountIn));
+
+        await MyDefiProject.swapExactOutputMultiHop(parseEther(wethAmountOut),parseEther(daiAmountInMaximum));
     }
 }
 
